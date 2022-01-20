a825e4c930a8d19c9e02c2ffb84cabc989f78470
ambiesoft <ambiesoft.trueff@gmail.com>
2022-01-20 13:29:08 +0900

    Lambda test

--- basic/src/com/ambiesoft/intelliJJavaTest/basic/Main.java
+++ basic/src/com/ambiesoft/intelliJJavaTest/basic/Main.java
@@ -1,8 +1,22 @@
 package com.ambiesoft.intelliJJavaTest.basic;
 
 public class Main {
-
+    public static void testFunc(int val) {
+        val = 150;
+        System.out.println(val);
+    }
+    public static void runLambda(Runnable runnable) {
+        runnable.run();
+    }
     public static void main(String[] args) {
 	    System.out.println("Hello World!");
+        testFunc(100);
+
+        String msg = "Hello Lambda!";
+        Runnable runner = () -> { System.out.println(msg); };
+        // java: local variables referenced from a lambda expression must be final or effectively final
+        // msg = "Hello Lambda ver2!";
+
+        runLambda(runner);
     }
 }
